921ab16ccbf0789a244792ae77ae5856f74e17ab
Milad Irannejad <moorara@gmail.com>
2020-01-13 01:15:51 -0500

    Update terraform project

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,8 +15,8 @@ terraform {
 # https://www.terraform.io/docs/configuration/providers.html#provider-versions
 # https://www.terraform.io/docs/configuration/terraform.html
 provider "aws" {
-  # Equivalent to ">= 2.43.0, < 3.0.0"
-  version    = "~> 2.43"
+  # Equivalent to ">= 2.44.0, < 3.0.0"
+  version    = "~> 2.44"
   access_key = var.access_key
   secret_key = var.secret_key
   region     = var.region
@@ -29,30 +29,29 @@ provider "aws" {
 module "network" {
   source = "../modules/network"
 
-  name        = local.name
-  region      = var.region
-  az_count    = 3
-  common_tags = local.common_tags
-  region_tag  = local.region_tag
-
-  vpc_tags = {
-    "kubernetes.io/cluster/${local.name}" = "shared"
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.name}" = "shared"
-  }
+  name                = local.name
+  region              = var.region
+  az_count            = 3
+  enable_bastion      = true
+  bastion_key_name    = local.ssh_key_name
+  common_tags         = local.common_tags
+  region_tag          = local.region_tag
+  vpc_tags            = local.kubernetes_tags
+  private_subnet_tags = local.kubernetes_tags
+  public_subnet_tags  = local.kubernetes_tags
 }
 
 module "cluster" {
   source = "../modules/cluster"
 
-  name        = local.name
-  region      = var.region
-  vpc_id      = module.network.vpc.id
-  subnet_ids  = [ for subnet in module.network.private_subnets: subnet.id ]
-  common_tags = local.common_tags
-  region_tag  = local.region_tag
+  name               = local.name
+  region             = var.region
+  vpc_id             = module.network.vpc.id
+  subnet_ids         = [ for subnet in module.network.public_subnets: subnet.id ]
+  ssh_key_name       = local.ssh_key_name
+  enable_node_groups = true
+  common_tags        = local.common_tags
+  region_tag         = local.region_tag
 }
 
 # ================================================================================
@@ -64,6 +63,7 @@ locals {
 }
 
 # Configure kubectl
+# https://www.terraform.io/docs/configuration/expressions.html#string-literals
 # https://www.terraform.io/docs/provisioners/null_resource.html
 # https://www.terraform.io/docs/provisioners/index.html
 # https://www.terraform.io/docs/provisioners/local-exec.html
@@ -73,24 +73,25 @@ resource "null_resource" "configure_kubectl" {
   ]
 
   provisioner "local-exec" {
-    command = <<EOF
+    command = <<-EOT
       aws eks update-kubeconfig --region ${var.region} --name ${local.name}
       sed -i '' "s/${local.cluster_arn}/${local.name}/g" ~/.kube/config
-    EOF
+    EOT
   }
 }
 
 # Clean up kubectl configurations
+# https://www.terraform.io/docs/configuration/expressions.html#string-literals
 # https://www.terraform.io/docs/provisioners/null_resource.html
 # https://www.terraform.io/docs/provisioners/index.html#destroy-time-provisioners
 # https://www.terraform.io/docs/provisioners/local-exec.html
 resource "null_resource" "cleanup_kubectl" {
   provisioner "local-exec" {
     when    = destroy
-    command = <<EOF
+    command = <<-EOT
       kubectl config unset clusters.${local.name}
       kubectl config unset users.${local.name}
       kubectl config unset contexts.${local.name}
-    EOF
+    EOT
   }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -24,18 +24,3 @@ output "cluster_status" {
   value       = module.cluster.status
   description = "The status of the Kubernetes cluster."
 }
-
-output "cluster_endpoint" {
-  value       = module.cluster.endpoint
-  description = "The endpoint for API server of the Kubernetes cluster."
-}
-
-output "cluster_certificate_authority" {
-  value       = module.cluster.certificate_authority
-  description = "The certificate authority data for the Kubernetes cluster (base64-encoded)."
-}
-
-output "kubeconfig" {
-  value       = module.cluster.kubeconfig
-  description = "The kubectl configuration for accessing the cluster."
-}
